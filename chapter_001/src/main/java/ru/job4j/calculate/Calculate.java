c96f5f67b697824c7977c1bc6931f06f43dcff17
Sergey Shpakovsky <shpaser3@yandex.ru>
2018-11-18 19:27:50 +0300

    1.3. JavaDoc. Code convention[#86887]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,18 @@
 package ru.job4j.calculate;
 
+
+/**
+* Class Класс выводящий в консоль строку "Hello world"
+* @author Sergey Shpakovsky
+* @since 16.11.2018
+* @version 1
+*/
 public class Calculate
-{
+{	
+	/**
+	* Main. Вывод в консоль "Hello world".
+	* @param args - args.
+	*/
 	public static void main(String[] args)
 	{
 		System.out.println("Hello world");
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,7 @@
+/**
+* Package for calculate task.
+*
+* @author Sergey Shpakovsky (shpaser2@yandex.ru)
+* @version $Id$
+* @since 14.11.2018
+*/
