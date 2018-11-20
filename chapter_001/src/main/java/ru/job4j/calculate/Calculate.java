9d4fb08c38f29176d149c323e02729822dd605d1
Sergey Shpakovsky <shpaser3@yandex.ru>
2018-11-20 18:17:36 +0300

    2.5. Инспектор кода IDEA.[#86847] Исправил "URI is not registered" выбрав "fetch external resourse" в checkstyle.xml на 4й строке.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,26 +1,25 @@
 package ru.job4j.calculate;
 
-
 /**
-* Class Return on console "Hello world" plus echo with writed name.
-* @author Sergey Shpakovsky
-* @since 16.11.2018
-* @version 1
-*/
-public class Calculate {	
+ * Class Return on console "Hello world" plus echo with name.
+ * @author Sergey Shpakovsky
+ * @since 16.11.2018
+ * @version 1
+ */
+public class Calculate {
 	/**
-	* Main. Вывод в консоль "Hello world".
-	* @param args - args.
-	*/
+	 * Main. Вывод в консоль "Hello world".
+	 * @param args - args.
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
-	
+
 	/**
-	* Method echo.
-	* @param name Your name.
-	* @return Echo plus your name.
-	*/
+	 * Method echo.
+	 * @param name Your name.
+	 * @return Echo plus your name.
+	 */
 	public String echo(String name) {
 		return "Echo, echo, echo : " + name;
 	}
