739547b94c8f8e528d7f2f250677d9308bfc2ef5
Sergey Shpakovsky <shpaser3@yandex.ru>
2018-11-25 18:05:19 +0300

    4.1. Глупый бот.[#86850]

---
+++ chapter_001/src/test/java/ru/job4j/condition/DummyBotTest.java
@@ -0,0 +1,49 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Sergey Shpakovsky (shpaser2@yandex.ru)
+ * @version $Id$
+ * @since 25.11.2018
+ */
+public class DummyBotTest {
+    /**
+     * Test first condition.
+     */
+    @Test
+    public void whenGreetBot() {
+        DummyBot bot = new DummyBot();
+        assertThat(
+                bot.answer("Привет, Бот."),
+                is("Привет, умник.")
+        );
+    }
+
+    /**
+     * Test second condition.
+     */
+    @Test
+    public void whenByuBot() {
+        DummyBot bot = new DummyBot();
+        assertThat(
+                bot.answer("Пока."),
+                is("До скорой встречи.")
+        );
+    }
+
+    /**
+     * Test third condition.
+     */
+    @Test
+    public void whenUnknownBot() {
+        DummyBot bot = new DummyBot();
+        assertThat(
+                bot.answer("Сколько будет 2 + 2?"),
+                is("Это ставит меня в тупик. Спросите другой вопрос.")
+        );
+    }
+}
