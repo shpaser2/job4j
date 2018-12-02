5236be5bcdb5cbf2dfc7d529580c743bd2bc1b70
Sergey Shpakovsky <shpaser3@yandex.ru>
2018-12-02 19:59:43 +0300

    5.4. Построить пирамиду в псевдографике.[#86861]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -0,0 +1,84 @@
+package ru.job4j.loop;
+
+/**
+ * Paint pyramid with selected height by backspaces and '^'.
+ *
+ * @author Sergey Shpakovsky (shpaser2@yandex.ru)
+ * @version $Id$
+ * @since 2.12.2018
+ */
+public class Paint {
+    /**
+     * Paint right part of pyramid.
+     *
+     * @param height pyramid
+     * @return right part of pyramid
+     */
+    public String rightTrl(int height) {
+        // Буфер для результата.
+        StringBuilder screen = new StringBuilder();
+        // ширина будет равна высоте.
+        //int weight = height;
+        // внешний цикл двигается по строкам.
+        for (int row = 0; row != height; row++) {
+            // внутренний цикл определяет положение ячейки в строке.
+            for (int column = 0; column != height; column++) {
+                // если строка равна ячейки, то рисуем галку.
+                // в данном случае строка определяем, сколько галок будет на строке
+                if (row >= column) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            // добавляем перевод строки.
+            screen.append(System.lineSeparator());
+        }
+        // Получаем результат.
+        return screen.toString();
+    }
+
+    /**
+     * Paint left part of pyramid.
+     *
+     * @param height pyramid
+     * @return left part of pyramid
+     */
+    public String leftTrl(int height) {
+        StringBuilder screen = new StringBuilder();
+        //int weight = height;
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != height; column++) {
+                if (row >= height - column - 1) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(System.lineSeparator());
+        }
+        return screen.toString();
+    }
+
+    /**
+     * Paint left an right part of pyramid together.
+     *
+     * @param height of pyramid
+     * @return pyramid
+     */
+    public String pyramid(int height) {
+        StringBuilder screen = new StringBuilder();
+        int weight = 2 * height - 1;
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != weight; column++) {
+                if (row >= height - column - 1 && row + height - 1 >= column) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(System.lineSeparator());
+        }
+        return screen.toString();
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -0,0 +1,78 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import java.util.StringJoiner;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Test pyramid paint: left, right and both parts.
+ *
+ * @author Sergey Shpakovsky (shpaser2@yandex.ru)
+ * @version $Id$
+ * @since 02.12.2018
+ */
+public class PaintTest {
+    /**
+     * Test right pyramid part paint.
+     */
+    @Test
+    public void whenPyramid4Right() {
+        Paint paint = new Paint();
+        String rst = paint.rightTrl(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("^   ")
+                                .add("^^  ")
+                                .add("^^^ ")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+
+    /**
+     * Test left pyramid part paint.
+     */
+    @Test
+    public void whenPyramid4Left() {
+        Paint paint = new Paint();
+        String rst = paint.leftTrl(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   ^")
+                                .add("  ^^")
+                                .add(" ^^^")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+
+
+    /**
+     * Test all pyramid paint.
+     */
+    @Test
+    public void whenPyramid4All() {
+        Paint paint = new Paint();
+        String rst = paint.pyramid(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   ^   ")
+                                .add("  ^^^  ")
+                                .add(" ^^^^^ ")
+                                .add("^^^^^^^")
+                                .toString()
+                )
+        );
+    }
+}
