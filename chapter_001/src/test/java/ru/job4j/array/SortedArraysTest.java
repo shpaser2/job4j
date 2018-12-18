84d1c9c14590e61a294ed94e3ced12faa82923d5
Sergey Shpakovsky <shpaser3@yandex.ru>
2018-12-18 23:12:49 +0300

    add some tests

--- chapter_001/src/test/java/ru/job4j/array/SortedArraysTest.java
+++ chapter_001/src/test/java/ru/job4j/array/SortedArraysTest.java
@@ -24,4 +24,31 @@ public class SortedArraysTest {
         int[] result = addTwoArrays.AddArrays(first, second);
         assertThat(result, is(expect));
     }
+
+
+    /**
+     * Test creation of one sorted arrays from two sorted without additional sort.
+     */
+    @Test
+    public void addTwoSortedArraysTogetherAndLeftSortedFirstArrayBigger() {
+        int[] first = {0, 1, 5, 6};
+        int[] second = {1, 2, 3};
+        int[] expect = {0, 1, 1, 2, 3, 5, 6};
+        SortedArrays addTwoArrays = new SortedArrays();
+        int[] result = addTwoArrays.AddArrays(first, second);
+        assertThat(result, is(expect));
+    }
+
+    /**
+     * Test creation of one sorted arrays from two sorted without additional sort.
+     */
+    @Test
+    public void addTwoSortedArraysTogetherAndLeftSortedSecondArrayBigger() {
+        int[] first = {-1};
+        int[] second = {0, 1, 2, 3, 5, 6};
+        int[] expect = { -1, 0, 1, 2, 3, 5, 6};
+        SortedArrays addTwoArrays = new SortedArrays();
+        int[] result = addTwoArrays.AddArrays(first, second);
+        assertThat(result, is(expect));
+    }
 }
