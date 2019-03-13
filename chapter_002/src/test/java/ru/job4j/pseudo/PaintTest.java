731cb8ed7db7f5aa5158c0c648ddaf3fbf44f640
Sergey Shpakovsky <shpaser3@yandex.ru>
2019-03-13 20:23:45 +0300

    Отрефакторил немного PaintTest - попробовал вынести дублируемый код с помощью тегов @Before @After .

--- chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.pseudo;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -10,61 +12,56 @@ import static org.junit.Assert.assertThat;
  * Заменой стандартного вывода проверяем результат вывода в классе Paint.
  *
  * @author Sergey Shpakovsky
- * @version 0.1
- * @since 10.03.2019
+ * @version 0.2
+ * @since 13.03.2019
  */
 public class PaintTest {
+    // поле содержит дефолтный вывод в консоль.
+    private final PrintStream stdout = System.out;
+    // буфер для результата.
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
     @Test
     public void whenDrawSquare() {
-        // получаем ссылку на стандартный вывод в консоль.
-        PrintStream stdout = System.out;
-        // Создаем буфер для хранения вывода.
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        //Заменяем стандартный вывод на вывод в пямять для тестирования.
-        System.setOut(new PrintStream(out));
-        // выполняем действия пишушиее в консоль.
         new Paint().draw(new Square());
-        // проверяем результат вычисления
         assertThat(
-                new String(out.toByteArray()),
-                is(
-                        new StringBuilder()
-                                .append("++++")
-                                .append("+     +")
-                                .append("+     +")
-                                .append("++++")
-                                .append(System.lineSeparator())
-                                .toString()
+                this.out.toString(),
+                is(new StringBuilder()
+                        .append("++++")
+                        .append("+     +")
+                        .append("+     +")
+                        .append("++++")
+                        .append(System.lineSeparator())
+                        .toString()
                 )
         );
-        // возвращаем обратно стандартный вывод в консоль.
-        System.setOut(stdout);
     }
 
     @Test
     public void whenDrawTriangle() {
-        // получаем ссылку на стандартный вывод в консоль.
-        PrintStream stdout = System.out;
-        // Создаем буфер для хранения вывода.
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        //Заменяем стандартный вывод на вывод в пямять для тестирования.
-        System.setOut(new PrintStream(out));
-        // выполняем действия пишушиее в консоль.
         new Paint().draw(new Triangle());
-        // проверяем результат вычисления
         assertThat(
-                new String(out.toByteArray()),
-                is(
-                        new StringBuilder()
-                                .append("+")
-                                .append("++")
-                                .append("+++")
-                                .append("++++")
-                                .append(System.lineSeparator())
-                                .toString()
+                this.out.toString(),
+                is(new StringBuilder()
+                        .append("+")
+                        .append("++")
+                        .append("+++")
+                        .append("++++")
+                        .append(System.lineSeparator())
+                        .toString()
                 )
         );
-        // возвращаем обратно стандартный вывод в консоль.
-        System.setOut(stdout);
     }
 }
