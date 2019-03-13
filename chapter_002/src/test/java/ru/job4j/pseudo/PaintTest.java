9e00d93ab780be6d45523fc19824d62867fa1370
Sergey Shpakovsky <shpaser3@yandex.ru>
2019-03-13 19:54:49 +0300

    добавил тест для Paint

--- chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -40,4 +40,31 @@ public class PaintTest {
         // возвращаем обратно стандартный вывод в консоль.
         System.setOut(stdout);
     }
+
+    @Test
+    public void whenDrawTriangle() {
+        // получаем ссылку на стандартный вывод в консоль.
+        PrintStream stdout = System.out;
+        // Создаем буфер для хранения вывода.
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        //Заменяем стандартный вывод на вывод в пямять для тестирования.
+        System.setOut(new PrintStream(out));
+        // выполняем действия пишушиее в консоль.
+        new Paint().draw(new Triangle());
+        // проверяем результат вычисления
+        assertThat(
+                new String(out.toByteArray()),
+                is(
+                        new StringBuilder()
+                                .append("+")
+                                .append("++")
+                                .append("+++")
+                                .append("++++")
+                                .append(System.lineSeparator())
+                                .toString()
+                )
+        );
+        // возвращаем обратно стандартный вывод в консоль.
+        System.setOut(stdout);
+    }
 }
